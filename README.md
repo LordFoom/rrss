e46aff9f0513b65d61efe06b807569d2b3b46ac3
lordfoom <lordfoom@gmail.com>
2024-04-15 18:08:07 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+#RRSS
+
+A 'ritten-in-Rust rss reader. 
+
+##Why?
+
+As I age, I tend more and more towards "Folk".
+
+Community controlled news|software|restoration|corporations
+
+This is my own folk rss reader software.
---
+++ src/model.rs
@@ -0,0 +1,21 @@
+///Big rss wrapping tag
+#[derive(Serialize, Deserialize)]
+struct Rss {
+    version: String,
+}
+
+///Rss Channel
+#[derive(Serialize, Deserialize)]
+struct Channel {
+    title: String,
+    link: String,
+    description: String,
+}
+
+///News items in the channel
+#[derive(Serialize, Deserialize)]
+struct Item {
+    title: String,
+    link: String,
+    description: String,
+}
