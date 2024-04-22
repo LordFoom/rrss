75b2743970b2748c1ce8e07fbdfead5742dacf73
Timothy Spring <foom@amazon.com>
2024-04-22 17:27:21 +0200

    Print out error when deserializing fails
    How do i handle double links with diff namespaces :thinking:

--- src/api.rs
+++ src/api.rs
@@ -9,9 +9,8 @@ pub async fn fetch_rss_feed(url: &str) -> Result<Option<Channel>> {
     println!("Text returned from the url: {}", txt);
     let mut de = serde_xml_rs::Deserializer::new_from_reader(txt.as_bytes())
         .non_contiguous_seq_elements(true);
-    if let Ok(rss) = Rss::deserialize(&mut de) {
-        return Ok(Some(rss.channel));
-    } else {
-        return Ok(None);
+    match Rss::deserialize(&mut de) {
+        Ok(rss) => return Ok(Some(rss.channel)),
+        Err(e) => panic!("Failed to deserialize! {}", e),
     }
 }
