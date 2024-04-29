859ec6db73f4e3b421a7209ed93f9f05cf911443
Timothy Spring <foom@amazon.com>
2024-04-29 18:03:02 +0200

    Finish setup of terminal method

--- src/tui.rs
+++ src/tui.rs
@@ -1,7 +1,15 @@
-use anyhow::Result;
-use crossterm::terminal::enable_raw_mode;
+use std::io::{self, Stdout};
 
-pub fn setup_terminal() -> Result<()> {
-    enable_raw_mode()?;
-    Ok(())
+use anyhow::{Context, Result};
+use crossterm::{
+    execute,
+    terminal::{enable_raw_mode, EnterAlternateScreen},
+};
+use ratatui::{backend::CrosstermBackend, Terminal};
+
+pub fn setup_terminal() -> Result<Terminal<CrosstermBackend<Stdout>>> {
+    let mut stdout = io::stdout();
+    let term = enable_raw_mode().context("Unable to enable raw mode")?;
+    execute!(stdout, EnterAlternateScreen).context("Alternate screen switch...FAILED")?;
+    Terminal::new(CrosstermBackend::new(stdout)).context("Could not create the terminal")
 }
