35707689bb0b4ca1081b0d83d05c8c91d9ab0089
Timothy Spring <foom@amazon.com>
2024-05-29 15:17:09 +0200

    Add tests for toml channel config
    Also rename Config to RssConfig,
    kept importing other Config classes from dependencies

--- src/config.rs
+++ src/config.rs
@@ -5,17 +5,17 @@ use std::{collections::HashMap, fs::read_to_string, path::Path};
 use crate::model::App;
 
 #[derive(Deserialize, Serialize)]
-struct Config {
+pub struct RssConfig {
     ///channel name to url map
     channels: HashMap<String, String>,
 }
 ///Load config from file, if file exists
 ///If no file passed, will default to checking for './.rrss.toml'
-pub fn load_config(path: Option<String>) -> Result<Option<Config>> {
+pub fn load_config(path: Option<String>) -> Result<Option<RssConfig>> {
     let config_file = if let Some(fp) = path {
         fp
     } else {
-        ".rrss.toml".to_owned()
+        ".rrss.toml".to_string()
     };
     let maybe_config = match Path::new(&config_file).exists() {
         true => {
@@ -30,18 +30,22 @@ pub fn load_config(path: Option<String>) -> Result<Option<Config>> {
 }
 
 pub fn save_config(path: Option<String>, app: &App) -> Result<()> {
+    let config_file = if let Some(fp) = path {
+        fp
+    } else {
+        ".rrss.toml".to_string()
+    };
     let mut channel_map = HashMap::new();
-    app.channels
-        .channels
-        .iter()
-        .for_each(|channel| {
-            channel_map.insert(channel.title, channel.get_link());
-        } );
-    //TODO actual saving
+    app.channels.channels.iter().for_each(|channel| {
+        channel_map.insert(channel.title.clone(), channel.get_link());
+    });
 
-
+    let config = RssConfig {
+        channels: channel_map,
+    };
+    let config_file_contents = toml::to_string(&config)?;
+    std::fs::write(config_file, config_file_contents)?;
     Ok(())
-
 }
 
 #[cfg(test)]
@@ -49,9 +53,25 @@ mod test {
     use super::*;
     //TODO test to check config creation
     //
+    #[test]
     pub fn test_load_config() {
         //create our test toml file
         let toml_str = r"[channels]
-Between Two Cairns"
+'Between Two Cairns'='https://feeds.buzzsprout.com/2042709.rss'
+'Fear of a Black Dragon'='http://feeds.libsyn.com/103241/rss'";
+        let test_file = "test_file.toml";
+        std::fs::write(test_file, toml_str).unwrap();
+        let cfg = load_config(Some(test_file.to_string())).unwrap().unwrap();
+        assert!(cfg.channels.len() == 2);
+        let two_cairns_found = cfg.channels.iter().any(|(k, _)| {
+            println!("key={k}");
+            return k == "Between Two Cairns";
+        });
+        let fobdf = cfg.channels.iter().any(|(k, _)| {
+            println!("key={k}");
+            return k == "Fear of a Black Dragon";
+        });
+        assert!(two_cairns_found, "Did not find Between Two Cairns");
+        assert!(fobdf, "Did not find Fear of a Black Dragon");
     }
 }
