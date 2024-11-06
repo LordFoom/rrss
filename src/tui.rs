2c4ac4b2a11c87948bb0c275ce5630435a6c9da2
Timothy Spring <foom@amazon.com>
2024-11-06 17:29:08 +0200

    Trying to do some space replacement
    got lost somewhere in the replacement of whitespace in filenames. i am going back to simple replace i think.

--- src/tui.rs
+++ src/tui.rs
@@ -1,6 +1,7 @@
 use clipboard::{ClipboardContext, ClipboardProvider};
 use log::{error, info};
-use regex::Regex;
+use regex::{bytes::Regex, Regex};
+use tempfile::tempdir;
 use std::{
     collections::HashMap,
     io::{self, stdout, Stdout},
@@ -380,12 +381,33 @@ pub async fn download_selected<'a>(app: &App<'a>) -> Result<()> {
     //get the url
     if let Some(item) = app.get_selected_item() {
         if let Some(url) = &item.link {
+            //temp directory
+            let tmp_dir = tempdir()?;
+            let file_name = if let Some(titles) = item.title {
+                if titles.is_empty() {
+                    "Unknown".to_string()
+                } else {
+                    if titles.get(0).is_none(){
+                        "Unknown".to_string()
+                    } else {
+                        let title = titles[0];
+                        let rgx = Regex::new(r"\s+").unwrap();
+                        let new_title = rgx.replace_all(&title, "-").unwrap();
+                        new_title
+                    }
+                }
+            }else {
+                "No title vec".to_string()
+            };
+            let pod_file = tmp_dir.path().join(item.title)
             //connect
+            //
             //download
             let pod = reqwest::get(url).await?;
             //check the status
             let status = pod.status();
             if status.is_success() {
+                let bytes = pod.bytes().await?
             } else {
                 let text = pod.text().await?;
                 let err_msg = format!(
